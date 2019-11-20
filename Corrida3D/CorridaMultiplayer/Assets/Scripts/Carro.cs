9b14e2ae19d3d0c18f2562187911be420d220dfc
NaejNunes <jeannobreganunes@hotmail.com>
2019-11-19 22:31:40 -0300

    atualizacao

---
+++ Corrida3D/CorridaMultiplayer/Assets/Scripts/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carro : MonoBehaviour
+{
+    public float velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+        }
+    }
+}
---
+++ JogoEmRede/Assets/Script/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public Material[] skin;
+    public float velocidade, velocidadeRotate;
+    public GameObject tiro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int n = Random.Range(0, skin.Length);
+        this.gameObject.GetComponent<Renderer>().material = skin[n];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLocalPlayer)
+        {
+            if(Input.GetAxis("Horizontal") > 0)
+             {
+                 transform.Rotate(Vector3.up * velocidadeRotate * Time.deltaTime);
+             }
+
+            if(Input.GetAxis("Horizontal") < 0)
+            {
+               transform.Rotate(Vector3.up * -velocidadeRotate * Time.deltaTime);
+            }
+
+            if(Input.GetAxis("Vertical") > 0)
+            {
+                transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+            }
+
+            if(Input.GetAxis("Vertical") < 0)
+            {
+                transform.Translate(Vector3.back * velocidade * Time.deltaTime);
+            }
+        }
+        
+    }
+}
